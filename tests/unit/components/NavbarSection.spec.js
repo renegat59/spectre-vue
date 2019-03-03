94b77d02270a05fc64fdbda6d62690e9947b11ec
Mateusz P <bananq@gmail.com>
2019-03-03 20:46:32 +0100

    added navbar section test

---
+++ tests/unit/components/NavbarSection.spec.js
@@ -0,0 +1,43 @@
+import { shallowMount } from '@vue/test-utils';
+import NavbarSection from '@/components/NavbarSection.vue';
+
+describe('NavbarSection.vue', () => {
+  describe('basic NavbarSection rendering', () => {
+    let wrapper;
+    beforeAll(() => {
+      wrapper = shallowMount(NavbarSection, {
+        slots: {
+          default: '<a href="#">Link</a>'
+        }
+      });
+    });
+
+    it("Has class 'navbar-section'", () => {
+      expect(wrapper.classes()).toContain('navbar-section');
+    });
+
+    it("Has a default slot", () => {
+      expect(wrapper.findAll("a").length).toBe(1);
+    });
+
+  });
+
+  it("Has a 'tag' prop to set the navbar section HTML tag", () => {
+    const wrapper = shallowMount(NavbarSection, {
+      propsData: {
+        tag: 'div'
+      }
+    });
+    expect(wrapper.element.nodeName).toBe('DIV');
+  });
+
+  it("Has a 'center' boolean prop to set the section in the center", () => {
+    const wrapper = shallowMount(NavbarSection, {
+      propsData: {
+        center: true
+      }
+    });
+    expect(wrapper.findAll('.navbar-center').length).toBe(1);
+  });
+
+});
