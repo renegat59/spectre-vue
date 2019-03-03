94eccf2eaa20ad73438b3429c583bb7d17161449
Mateusz P <bananq@gmail.com>
2019-03-03 13:28:11 +0100

    directives moved to a folder

---
+++ src/directives/index.js
@@ -0,0 +1,4 @@
+import ShowOn from './show-on';
+import HideOn from './hide-on';
+
+export default { ShowOn, HideOn };
---
+++ src/directives/show-on.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import { screenSizes } from '../consts';
+
+//the directives should look like v-show-on.xs.xl and v-hide-on.xs.xl
+
+Vue.directive('show-on', {
+  bind(el, binding, vnode) {
+    Object.keys(binding.modifiers)
+      .filter(modifier => screenSizes.indexOf(modifier) !== -1)
+      .forEach(modifier => el.classList.add(`show-${modifier}`));
+  }
+});
