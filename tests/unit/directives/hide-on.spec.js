ff5d6e547efe889c26f2d22983264d50992cebc1
Mateusz P <bananq@gmail.com>
2019-03-03 13:30:08 +0100

    moved directives test to a separate folder

---
+++ tests/unit/directives/hide-on.spec.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import '@/directives';
+
+describe('Directive v-hide-on', () => {
+  it('should hide on XS', () => {
+    const vm = new Vue({
+      template: '<div><span v-hide-on.xs>hello</span></div>'
+    }).$mount();
+    expect(vm.$el.innerHTML).toBe('<span class="hide-xs">hello</span>');
+  })
+});
