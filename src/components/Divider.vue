f25edf65be2d49d86ae0ea30b152649c139bea1c
Mateusz P <bananq@gmail.com>
2019-03-24 23:59:14 +0100

    Added Divider
    ADded Divider componenet and the simple tests for it.

--- src/components/Divider.vue
+++ src/components/Divider.vue
@@ -1,9 +1,15 @@
 <template>
-  <div></div>
+  <div :class="`divider${this.vertical ? '-vert' : ''}`" :data-content="content"></div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    vertical: {
+      type: Boolean,
+      default: false
+    },
+    content: String
+  }
 };
 </script>
---
+++ tests/unit/components/Divider.spec.js
@@ -0,0 +1,22 @@
+import { shallowMount } from '@vue/test-utils';
+import Divider from '@/components/Divider.vue';
+
+describe("Divider.vue", () => {
+
+  it("Renders div with divider class", () => {
+    const wrapper = shallowMount(Divider);
+    expect(wrapper.is('div')).toBe(true);
+    expect(wrapper.classes()).toContain('divider');
+    wrapper.setProps({vertical: true});
+    expect(wrapper.classes()).toContain('divider-vert');
+  });
+
+  it("Has the data attribute to show the content", () => {
+    const wrapper = shallowMount(Divider, {
+      propsData: {
+        content: 'Hello'
+      }
+    });
+    expect(wrapper.attributes('data-content')).toBe('Hello');
+  })
+});
